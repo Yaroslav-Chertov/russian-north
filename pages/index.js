43463d01a251c09eb115bd6c22b426e281b0f793
Yaroslav Chertov <yachertov@yandex.ru>
2024-01-28 18:18:05 +0300

    сделать хэдр и лид

---
+++ pages/index.js
@@ -0,0 +1,155 @@
+'use strict';
+
+console.clear();
+
+class Grain {
+    constructor(el) {
+        /**
+         * Options
+         * Increase the pattern size if visible pattern
+         */
+        this.patternSize = 150;
+        this.patternScaleX = 1;
+        this.patternScaleY = 1;
+        this.patternRefreshInterval = 3; // 8
+        this.patternAlpha = 15; // int between 0 and 255,
+
+        /**
+         * Create canvas
+         */
+        this.canvas = el;
+        this.ctx = this.canvas.getContext('2d');
+        this.ctx.scale(this.patternScaleX, this.patternScaleY);
+
+        /**
+         * Create a canvas that will be used to generate grain and used as a
+         * pattern on the main canvas.
+         */
+        this.patternCanvas = document.createElement('canvas');
+        this.patternCanvas.width = this.patternSize;
+        this.patternCanvas.height = this.patternSize;
+        this.patternCtx = this.patternCanvas.getContext('2d');
+        this.patternData = this.patternCtx.createImageData(this.patternSize, this.patternSize);
+        this.patternPixelDataLength = this.patternSize * this.patternSize * 4; // rgba = 4
+
+        /**
+         * Prebind prototype function, so later its easier to user
+         */
+        this.resize = this.resize.bind(this);
+        this.loop = this.loop.bind(this);
+
+        this.frame = 0;
+
+        window.addEventListener('resize', this.resize);
+        this.resize();
+
+        window.requestAnimationFrame(this.loop);
+    }
+
+    resize() {
+        this.canvas.width = window.innerWidth * devicePixelRatio;
+        this.canvas.height = window.innerHeight * devicePixelRatio;
+    }
+
+    update() {
+        const { patternPixelDataLength, patternData, patternAlpha, patternCtx } = this;
+
+        // put a random shade of gray into every pixel of the pattern
+        for (let i = 0; i < patternPixelDataLength; i += 4) {
+            // const value = (Math.random() * 255) | 0;
+            const value = Math.random() * 255;
+
+            patternData.data[i] = value;
+            patternData.data[i + 1] = value;
+            patternData.data[i + 2] = value;
+            patternData.data[i + 3] = patternAlpha;
+        }
+
+        patternCtx.putImageData(patternData, 0, 0);
+    }
+
+    draw() {
+        const { ctx, patternCanvas, canvas, viewHeight } = this;
+        const { width, height } = canvas;
+
+        // clear canvas
+        ctx.clearRect(0, 0, width, height);
+
+        // fill the canvas using the pattern
+        ctx.fillStyle = ctx.createPattern(patternCanvas, 'repeat');
+        ctx.fillRect(0, 0, width, height);
+    }
+
+    loop() {
+        // only update grain every n frames
+        const shouldDraw = ++this.frame % this.patternRefreshInterval === 0;
+        if (shouldDraw) {
+            this.update();
+            this.draw();
+        }
+
+        window.requestAnimationFrame(this.loop);
+    }
+}
+
+/**
+ * Initiate Grain
+ */
+const el = document.querySelector('.grain');
+const grain = new Grain(el);
+
+///////////////////////
+// TextScramble
+class TextScramble {
+    constructor(element) {
+        this.element = element;
+        this.chars = "!<>-_\\/[]{}—=+*^?#________";
+        this.update = this.update.bind(this);
+    }
+    setText(newText) {
+        const oldText = this.element.innerText;
+        const length = Math.max(oldText.length, newText.length);
+        const promise = new Promise((resolve) => (this.resolve = resolve));
+        this.queue = [];
+        for (let i = 0; i < length; i++) {
+            const from = oldText[i] || "";
+            const to = newText[i] || "";
+            const start = Math.floor(Math.random() * 40);
+            const end = start + Math.floor(Math.random() * 40);
+            this.queue.push({ from, to, start, end });
+        }
+        cancelAnimationFrame(this.frameRequest);
+        this.frame = 0;
+        this.update();
+        return promise;
+    }
+    update() {
+        let output = "";
+        let complete = 0;
+        for (let i = 0, n = this.queue.length; i < n; i++) {
+            let { from, to, start, end, char } = this.queue[i];
+            if (this.frame >= end) {
+                complete++;
+                output += to;
+            } else if (this.frame >= start) {
+                if (!char || Math.random() < 0.28) {
+                    char = this.randomChar();
+                    this.queue[i].char = char;
+                }
+                output += `<span class="dud">${char}</span>`;
+            } else {
+                output += from;
+            }
+        }
+        this.element.innerHTML = output;
+        if (complete === this.queue.length) {
+            this.resolve();
+        } else {
+            this.frameRequest = requestAnimationFrame(this.update);
+            this.frame++;
+        }
+    }
+    randomChar() {
+        return this.chars[Math.floor(Math.random() * this.chars.length)];
+    }
+}
